ddb4057c204be462dfdbf821d750df7e4f419d42
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2020-12-17 12:59:25 -0800

    Added initial Spring Boot and Vue.js files

---
+++ src/main/java/org/webstory/ourstory/OurstoryApplication.java
@@ -0,0 +1,13 @@
+package org.webstory.ourstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/webstory/ourstory/OurstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webstory.ourstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
