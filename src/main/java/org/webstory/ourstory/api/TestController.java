dc0036e648e0dbe5adb8504ecf4bdba1362beb93
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2021-01-15 12:31:33 -0800

    Ammend. Add cors

--- src/main/java/org/webstory/ourstory/api/TestController.java
+++ src/main/java/org/webstory/ourstory/api/TestController.java
@@ -3,6 +3,7 @@ package org.webstory.ourstory.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.webstory.ourstory.model.Segment;
 import org.webstory.ourstory.model.Story;
 import org.webstory.ourstory.services.StoryService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/test") // This maps this whole controller to /test
 @RestController // This is essentially a macro for @controller and @responsebody, which
 						// basically tells Spring that this class handles backend API calls and
